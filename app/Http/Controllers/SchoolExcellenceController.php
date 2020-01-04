df38099d966129c0fc8f40e57dbdd8a6de892415
Kawsar Ahmed <kawsar.ahmed1001@gmail.com>
2020-01-04 15:42:49 +0600

    [model created SchoolExcellence]

---
+++ app/Http/Controllers/SchoolExcellenceController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\SchoolExcellence;
+use Illuminate\Http\Request;
+
+class SchoolExcellenceController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\SchoolExcellence  $schoolExcellence
+     * @return \Illuminate\Http\Response
+     */
+    public function show(SchoolExcellence $schoolExcellence)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\SchoolExcellence  $schoolExcellence
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(SchoolExcellence $schoolExcellence)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\SchoolExcellence  $schoolExcellence
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, SchoolExcellence $schoolExcellence)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\SchoolExcellence  $schoolExcellence
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(SchoolExcellence $schoolExcellence)
+    {
+        //
+    }
+}
